166f95910b79fcc115f6f4c08fd11ff6133c5eee
yjq13 <381882327@qq.com>
2018-11-30 19:06:16 +0800

    优化网络部分代码

--- Client/Assets/Script/GameStartManager.cs
+++ Client/Assets/Script/GameStartManager.cs
@@ -1,14 +1,49 @@
 using UnityEngine;
 using System.Collections;
 using NetWork;
+using easy_moba;
 
 public class GameStartManager : MonoBehaviour
 {
+    private bool is_web_connected = false;
     IEnumerator Start()
     {
-        yield return StartCoroutine(NetworkManager.Instance.GetWebSocket().Connect());
-        NetworkManager.Instance.Connect();
+        yield return StartCoroutine(NetworkManager.Instance.StartConnect());
+        is_web_connected = true;
+        SendTestMessage();
+    }
 
+    private void SendTestMessage()
+    {
+        down_msg msg = new down_msg()
+        {
+            svr_ts = 1543483207,
+            seq = 0,
+            err_code = new reply_err_code()
+            {
+                err_code = "Yingjiaqi is your daddy"
+            }
+        };
+        up_msg msg2 = new up_msg()
+        {
+            data_op = new req_data_op()
+            {
+                op = t_op.add
+            }
+        };
+        NetworkManager.Instance.SendMessage(msg2);
+    }
+
+    private void Update()
+    {
+        if (is_web_connected)
+        {
+            var data = NetworkManager.Instance.ReceiveMessage();
+            if(data != null)
+            {
+                Debug.Log(data.err_code);
+            }
+        }
     }
 }
 
--- Client/Assets/Script/NetWork/NetworkManager.cs
+++ Client/Assets/Script/NetWork/NetworkManager.cs
@@ -20,58 +20,75 @@ namespace NetWork
             web_socket = new WebSocket(new Uri("ws://114.116.91.235:30000"));
         }
 
-        public WebSocket GetWebSocket()
+
+        public IEnumerator StartConnect()
         {
-            return web_socket;
+            return web_socket.Connect();
         }
 
-
-        public void Connect()
+        public void SendMessage<T>(T msg)
         {
-            down_msg msg = new down_msg()
-            {
-                svr_ts = 1543483207,
-                seq = 0,
-                err_code = new reply_err_code()
-                {
-                    err_code = "Yingjiaqi is your daddy"
-                }
-            };
-            up_msg msg2 = new up_msg()
-            {
-                data_op = new req_data_op()
-                {
-                    op = t_op.add
-                }
-            };
-            MemoryStream ms = new MemoryStream();
-            Serializer.Serialize(ms, msg2);
+            MemoryStream memory_stream = new MemoryStream();
+            Serializer.Serialize(memory_stream, msg);
+            byte[] binary_msg = new byte[memory_stream.Length];
+            memory_stream.Position = 0;
+            memory_stream.Read(binary_msg, 0, binary_msg.Length);
 
-            byte[] result = new byte[ms.Length];
-            ms.Position = 0;
-            ms.Read(result, 0, result.Length);
-            for(int i = 0; i < result.Length; i++)
+            web_socket.Send(binary_msg);
+        }
+
+        //public void Connect()
+        //{
+        //    down_msg msg = new down_msg()
+        //    {
+        //        svr_ts = 1543483207,
+        //        seq = 0,
+        //        err_code = new reply_err_code()
+        //        {
+        //            err_code = "Yingjiaqi is your daddy"
+        //        }
+        //    };
+        //    up_msg msg2 = new up_msg()
+        //    {
+        //        data_op = new req_data_op()
+        //        {
+        //            op = t_op.add
+        //        }
+        //    };
+        //    MemoryStream ms = new MemoryStream();
+        //    Serializer.Serialize(ms, msg2);
+
+        //    byte[] result = new byte[ms.Length];
+        //    ms.Position = 0;
+        //    ms.Read(result, 0, result.Length);
+        //    for(int i = 0; i < result.Length; i++)
+        //    {
+        //        Debug.Log(result[i]);
+        //    }
+
+        //    byte[] test_result = new byte[] { 8, 199, 222, 254, 223, 5, 26, 19, 10, 17, 119, 104, 111, 32, 105, 115, 32, 121, 111, 117, 114, 32, 100, 97, 100, 100, 121 };
+
+        //    MemoryStream ms2 = new MemoryStream(result);
+
+        //    up_msg result2 = Serializer.Deserialize<up_msg>(ms2);
+
+        //    web_socket.Send(result);
+
+        //}
+
+        public down_msg ReceiveMessage()
+        {
+            byte[] recv_msg = web_socket.Recv();
+            if(recv_msg != null)
             {
-                Debug.Log(result[i]);
+                MemoryStream memory_stream = new MemoryStream(recv_msg);
+                down_msg rec_message = Serializer.Deserialize<down_msg>(memory_stream);
+                return rec_message;
             }
-
-            byte[] test_result = new byte[] { 8, 199, 222, 254, 223, 5, 26, 19, 10, 17, 119, 104, 111, 32, 105, 115, 32, 121, 111, 117, 114, 32, 100, 97, 100, 100, 121 };
-
-            MemoryStream ms2 = new MemoryStream(result);
-
-            up_msg result2 = Serializer.Deserialize<up_msg>(ms2);
-
-            web_socket.Send(result);
-
+            return null;
         }
 
 
-        void OnRemoveEvent()
-        {
-            //socketIO.Off(SocketIOProtocol.ProtocolLogin);
-            //socketIO.Off(SocketIOProtocol.ProtocolChat);
-            //socketIO.Off(SocketIOProtocol.ProtocolInfo);
-        }
 
     }
 }
