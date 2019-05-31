72e411b2b5ae40aed51f03a1beb6971f3afa6202
yjq13 <381882327@qq.com>
2019-05-31 20:00:24 +0800

    UIScenebase添加功能

--- Client/Assets/Script/Common/UI/UISceneBase.cs
+++ Client/Assets/Script/Common/UI/UISceneBase.cs
@@ -2,9 +2,17 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using UnityEngine;
 
 namespace Common
 {
+    enum UI_OPEN_TYPE
+    {
+        NORMAL = 0,
+        STACK,
+        QUEUE//队列形式展示，关闭一个展示一个
+    }
+
     public class UISceneBase
     {
         private Dictionary<UIBaseController,uint> m_allControllers;
@@ -16,6 +24,14 @@ namespace Common
             m_allControllers = new Dictionary<UIBaseController, uint>();
             m_queueControllers = new Queue<UIBaseController>();
             m_stackControllers = new Stack<UIBaseController>();
+        }
+
+        public UIBaseController OpenUIController<T>() where T : UIBaseController
+        {
+            UnityEngine.Object res = ResourceManager.Instance.GetResource("");
+            GameObject gameobj = GameObject.Instantiate(res) as GameObject;
+            T m_controller = gameobj.AddComponent(typeof(T)) as T;
+            return m_controller;
         }
     }
 }
